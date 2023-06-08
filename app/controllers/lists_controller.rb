f9e0aecb4c5310afe0c142f4b79eadf15528f98f
KyoheiUtsunomiya <134346889+StupidSheeep@users.noreply.github.com>
2023-06-08 04:46:25 +0000

    [Start] 3-3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,32 @@
 class ListsController < ApplicationController
   def new
-    # Viewへ渡すためのイン寸タンス変数に空のModelオブジェクトを生成
+    # 3-2 Viewへ渡すためのイン寸タンス変数に空のModelオブジェクトを生成
     @list = List.new
   end
 
+  #3-2 以下を追加
+  def create
+    #3-2 1.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    #3-2 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    #3-2 4. トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+
   def index
+    #@lists = List.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'lists/new' #データの取得(ページ自体もデータ)
-  post 'lists'  => 'lists#create' #(新しいデータの作成)
+  get 'lists/new' #3-2 データの取得(ページ自体もデータ)
+  get '/top' => 'homes#top'
+  post 'lists'  => 'lists#create' #3-2(新しいデータの作成)
+  get 'lists' => 'lists#index'
 
-
-  get 'lists/new'
-  get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
-  get '/top' => 'homes#top'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

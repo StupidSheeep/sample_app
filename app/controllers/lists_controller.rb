e02a3a533c7287853cd6920b6f202bb2754abc35
KyoheiUtsunomiya <134346889+StupidSheeep@users.noreply.github.com>
2023-06-10 04:51:35 +0000

    [Updata] 3-6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -35,6 +35,6 @@ class ListsController < ApplicationController
   private
   #3-2 ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end

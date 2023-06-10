e003917352ec901014d01d8b76c7ad0701b15826
KyoheiUtsunomiya <134346889+StupidSheeep@users.noreply.github.com>
2023-06-10 06:56:50 +0000

    [Start] 3-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  get 'lists/new' #3-2 データの取得(ページ自体もデータ)
-  get '/top' => 'homes#top'
-  post 'lists'  => 'lists#create' #3-2(新しいデータの作成)
-  get 'lists' => 'lists#index' #3-3
+  # get 'lists/new' #3-2 データの取得(ページ自体もデータ)
+  # get '/top' => 'homes#top'
+  # post 'lists'  => 'lists#create' #3-2(新しいデータの作成)
+  # get 'lists' => 'lists#index' #3-3
   #3-4 .../lists/1や...lists/3に該当する
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
-  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+  # get 'lists/:id' => 'lists#show', as: 'list'
+  # get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  # patch 'lists/:id' => 'lists#update', as: 'update_list'
+  # delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+
+  get 'top' => 'homes#top'
+  resources :lists
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
